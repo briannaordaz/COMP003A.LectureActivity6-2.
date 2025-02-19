39812c26e8fa12892cefd128fdda8f02dcb30890
briannaordaz <briannaordaz5@gmail.com>
2025-02-18 23:15:57 -0800

    implemented three overloaded 'CalculateArea' methods

---
+++ COMP003A.LectureActivity6-2/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace COMP003A.LectureActivity6_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
---
+++ COMP003A.LectureActivity6-2/ShapeCalculator.cs
@@ -0,0 +1,41 @@
+namespace COMP003A.LectureActivity6_2;
+
+/// <summary>
+/// Demonstrates method overloading for different shape calculations
+/// </summary>
+public class ShapeCalculator
+{
+    /// <summary>
+    /// Calculates the area of a circle.
+    /// </summary>
+    /// <param name="radius"></param>
+    /// <returns></returns>
+
+    public double CalculateArea(double radius)
+    {
+        return Math.PI * radius * radius;
+    }
+
+    /// <summary>
+    /// <param name="length"></param>
+    /// <param name="width"></param>
+    /// <returns></returns>
+
+    public double CalculateArea(double length, double width)
+    {
+        return length * width;
+    }
+
+    /// <summary>
+    /// Calculates the area of a triangle.
+    /// </summary>
+    /// <param name="baseLength"></param>
+    /// <param name="height"></param>
+    /// <param name="isTriangle"></param>
+    /// <return></return>
+
+    public double CalculateArea(double baseLength, double height, bool isTriangle)
+    {
+        return 0.5 * baseLength * height;
+    }
+}
