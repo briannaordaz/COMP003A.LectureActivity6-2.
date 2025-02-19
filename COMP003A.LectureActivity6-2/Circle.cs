f6ba839b6a1f0872e3de6e66552b35e6ca2132dc
briannaordaz <briannaordaz5@gmail.com>
2025-02-18 23:26:43 -0800

    created a derived class for Circle

---
+++ COMP003A.LectureActivity6-2/Circle.cs
@@ -0,0 +1,28 @@
+namespace COMP003A.LectureActivity6_2;
+
+/// <summary>
+/// Represents a circle with a specific area calculation.
+/// </summary>
+public class Circle
+{
+    // Auto-implemented property for the radius of the circle
+    public double Radius { get; set; }
+
+    /// <summary>
+    /// Constructor for the Circle class.
+    /// </summary>
+
+    public Circle(double radius)
+    {
+        Radius = radius;
+    }
+
+    /// <summary>
+    /// Displays the area of a circle.
+    /// </summary>
+
+    public override void DisplayArea()
+    {
+        Console.WriteLine($"Circle Area: {Math.PI * Radius * Radius}");
+    }
+}
